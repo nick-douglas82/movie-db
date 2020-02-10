f202f3e9bfddfc092f55798303b50311f4fd5346
Nick Douglas <n.douglas@ry.com>
2020-02-10 11:31:41 +0000

    Moved back to class components temporarily

--- src/components/movie/Listing.js
+++ src/components/movie/Listing.js
@@ -1,39 +1,59 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from 'axios';
 import MovieCard from '../movie/Card.js'
 
+class Listing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      movies: []
+    };
+  }
 
+  componentDidMount() {
+    axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=e0c577647a14eae09f07aa14fee7caeb&language=en-GB&page=1&region=GB")
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          movies: result.data.results
+        });
+      },
 
-const Listing = () => {
-  const [hasError, setErrors] = useState(false);
-  const [movies, setMovies] = useState({});
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
 
-  useEffect(() => {
-    function fetchData() {
-      axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=e0c577647a14eae09f07aa14fee7caeb&language=en-GB&page=1&region=GB")
-        .then(res => setMovies(res.data.results))
-        .catch(err => setErrors(err));
+  render() {
+    const { error, isLoaded, movies } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="Listing">
+          {movies.map((movie, index) => (
+            <MovieCard
+              title={movie.title}
+              genres={movie.genre_ids}
+              rating={movie.vote_average}
+              poster={movie.poster_path}
+              key={index}
+            ></MovieCard>
+          ))}
+        </div>
+      );
     }
-
-    fetchData();
-  }, []);
-
-
-  return (
-    <div className="Listing">
-      {console.log(movies)}
-      {movies.map((movie, index) => (
-        <MovieCard
-          title={movie.title}
-          genres={movie.genre_ids}
-          rating={movie.vote_average}
-          poster={movie.poster_path}
-          key={index}
-        ></MovieCard>
-      ))}
-      <span>Has error: {JSON.stringify(hasError)}</span>
-    </div>
-  );
+  }
 }
 
 export default Listing;
